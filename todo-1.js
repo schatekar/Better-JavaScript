ee88557c8191b3c3fa46282b5f53f3b50e01e53d
Suhas.Chatekar <suhas.chatekar@gmail.com>
2012-04-04 14:52:18 +0100

    Adding files used in better javascript post

---
+++ todo-1.js
@@ -0,0 +1,55 @@
+﻿$(document).ready(function () {
+
+    function getToDoHtml(todo) {
+        return '<li><span class="todo">' + todo + '</span><a class="edit">Edit</a><a class="remove">Remove</a></li>';
+    }
+
+    $('input[type="submit"].add-todo').click(function () {
+        var todo = $('input[type="text"].add-todo').val();
+
+        if (todo !== null) {
+
+            $('.todo-list').append(getToDoHtml(todo));
+            $('input[type="text"].add-todo').val(null);
+        }
+    });
+
+    $('a.edit').live('click', function () {
+
+        $(this).siblings('.remove').remove();
+        var todoSpan = $(this).siblings('span');
+        var todo = $(todoSpan).text();
+        var edittodo = '<input class="editing" type="text" data-original-value="' + todo + '" value="' + todo + '" />';
+        todoSpan.replaceWith(edittodo);
+
+        var actions = '<a class="save">Save</a><a class="cancel">Cancel</a>';
+        $(this).replaceWith(actions);
+    });
+
+    $('a.save').live('click', function () {
+
+        var todo = $(this).parent().children('.editing').val();
+
+        if (todo !== null) {
+            $(this).parent().replaceWith(getToDoHtml(todo));
+        }
+    });
+
+    $('a.cancel').live('click', function () {
+        var todo = $(this).parent().children('.editing').attr('data-original-value');
+
+        if (todo !== null) {
+            $(this).parent().replaceWith(getToDoHtml(todo));
+        }
+    });
+
+    $('a.remove').live('click', function () {
+
+        var confirmremove = confirm("Are you sure you want to remove this todo item?");
+
+        if (confirmremove) {
+            $(this).parent().remove();
+        }
+
+    });
+});
---
+++ todo-3.js
@@ -0,0 +1,57 @@
+﻿$(document).ready(function () {
+	var addTodoButton = $('input[type="submit"].add-todo');
+	var todoTextbox = $('input[type="text"].add-todo');
+	
+    $(addTodoButton).click(function () {
+        var todo = $(todoTextbox).val();
+
+        if (todo) {
+
+            $('.todo-list').append(HtmlFor(todo));
+            $(todoTextbox).val(null);
+        }
+    });
+	
+	function HtmlFor(todo) {
+        return '<li><span class="todo">' + todo + '</span><a class="edit">Edit</a><a class="remove">Remove</a></li>';
+    }
+
+    $('a.edit').live('click', function () {
+
+        $(this).siblings('.remove').remove();
+        var todoSpan = $(this).siblings('span');
+        var todo = $(todoSpan).text();
+        var edittodo = '<input class="editing" type="text" data-original-value="' + todo + '" value="' + todo + '" />';
+        todoSpan.replaceWith(edittodo);
+
+        var actions = '<a class="save">Save</a><a class="cancel">Cancel</a>';
+        $(this).replaceWith(actions);
+    });
+
+    $('a.save').live('click', function () {
+
+        var todo = $(this).parent().children('.editing').val();
+
+        if (todo !== null) {
+            $(this).parent().replaceWith(getToDoHtml(todo));
+        }
+    });
+
+    $('a.cancel').live('click', function () {
+        var todo = $(this).parent().children('.editing').attr('data-original-value');
+
+        if (todo !== null) {
+            $(this).parent().replaceWith(htmlFor(todo));
+        }
+    });
+
+    $('a.remove').live('click', function () {
+
+        var confirmremove = confirm("Are you sure you want to remove this todo item?");
+
+        if (confirmremove) {
+            $(this).parent().remove();
+        }
+
+    });
+});
